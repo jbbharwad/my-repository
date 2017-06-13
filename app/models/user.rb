0c053a54fb0d8cbac639f1a2e68f33428f6a41d9
ruby on rails project <jsbharvad@gmail.com>
2017-06-13 02:54:42 +0000

    ADD friendship andsearch functionality for friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,5 +21,30 @@ class User < ActiveRecord::Base
   return false unless stock
   user_stocks.where(stock_id: stock.id).exists?
   end
+  
+  def not_friends_with?(friend_id)
+  friendships.where(friend_id: friend_id).count < 1
+  end
+
+  def except_current_user(users)
+  users.reject {|user| user.id == self.id}
+  end
+
+  def self.search(param)
+  return User.none if param.blank?
+  param.strip!
+  param.downcase!
+  (email_matches(param)).uniq
+  end
+
+
+  def self.email_matches(param)
+  matches('email', param)
+  end
+
+  def self.matches(field_name, param)
+  where("lower(#{field_name}) like ?", "%#{param}%")
+  end
+  
 
 end
